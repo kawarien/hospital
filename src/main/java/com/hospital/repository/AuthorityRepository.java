b00d12dced4fedb5a8bc3667b6706feebb86e2d7
kawarien <ibrahimabbaadam@yahoo.fr>
2018-03-04 20:56:30 +0000

    Initial application generated by JHipster-4.13.3

---
+++ src/main/java/com/hospital/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.hospital.repository;
+
+import com.hospital.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/com/hospital/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.hospital.service.dto;
